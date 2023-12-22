827c61771c48936452ef44728075fa4bfe3f64f6
walkerchiu <chenjen.chiou@gmail.com>
2023-12-22 22:12:50 +0800

    Feat(Module): Add Money and TaxedMoney types

--- django_app_core/types.py
+++ django_app_core/types.py
@@ -1,6 +1,31 @@
 import graphene
 
 
+class Money(graphene.ObjectType):
+    amount = graphene.Float(description="Amount of money.", required=True)
+    currency = graphene.String(description="Currency code.", required=True)
+
+    class Meta:
+        description = "Represents amount of money in specific currency."
+
+
+class TaxedMoney(graphene.ObjectType):
+    currency = graphene.String(description="Currency code.", required=True)
+    gross = graphene.Field(
+        Money, description="Amount of money including taxes.", required=True
+    )
+    net = graphene.Field(
+        Money, description="Amount of money without taxes.", required=True
+    )
+    tax = graphene.Field(Money, description="Amount of taxes.", required=True)
+
+    class Meta:
+        description = (
+            "Represents a monetary value with taxes. In cases where taxes were not "
+            "applied, net and gross values will be equal."
+        )
+
+
 class TaskStatusType(graphene.ObjectType):
     done = graphene.List(graphene.String)
     error = graphene.List(graphene.String)
